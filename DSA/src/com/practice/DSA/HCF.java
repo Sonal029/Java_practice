5913beb1ff877ef493f06427300fc2c3c13e4d8a
shakambhari <shakambharihift36@gmail.com>
2023-04-06 11:21:13 +0530

    added

---
+++ DSA/src/com/practice/DSA/HCF.java
@@ -0,0 +1,25 @@
+package com.practice.DSA;
+
+import java.util.Scanner;
+
+public class HCF 
+{
+     public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter First number");
+		int a =sc.nextInt();
+		System.out.println("Enter Second number");
+		int b =sc.nextInt();
+		int min = Math.min(a,b);
+		int max=0;
+		for(int i=1;i<=min;i++)
+		{
+			if(a%i==0 && b%i==0)
+			{
+				max=Math.max(i, max);
+			}
+		}
+		System.out.println("HCF is"+max);
+	}
+}
