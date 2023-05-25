12f6643ec014c19fc015144f9ec66a565e03e43d
shakambhari <shakambharihift36@gmail.com>
2023-05-25 20:17:03 +0530

    added

--- CollegeStudentApp/src/main/java/dao/StudentDaoImpl.java
+++ CollegeStudentApp/src/main/java/dao/StudentDaoImpl.java
@@ -6,31 +6,85 @@ import entities.College;
 import entities.Student;
 import exceptions.CollegeException;
 import exceptions.StudentException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
+import utilities.EMUtil;
 
 public class StudentDaoImpl implements StudentDao{
 
 	@Override
-	public Student registerStudent(Student student) throws StudentException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public Student registerStudent(Student student) throws StudentException {
+        if (student == null) {
+            throw new StudentException("Student object is null.");
+        }
 
-	@Override
-	public Student getStudentFromStudentId(int studentId) throws StudentException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        EntityManager entityManager = EMUtil.provideEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
 
-	@Override
-	public List<Student> getAllActiveStudents() throws StudentException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        try {
+            transaction.begin();
+            entityManager.persist(student);
+            transaction.commit();
+            return student;
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw new StudentException("Failed to register student.");
+        } finally {
+            entityManager.close();
+        }
+    }
 
-	@Override
-	public College getCollegeFromStudentId(int studentId) throws CollegeException, StudentException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Student getStudentFromStudentId(int studentId) throws StudentException {
+        EntityManager entityManager = EMUtil.provideEntityManager();
 
+        try {
+            Student student = entityManager.find(Student.class, studentId);
+            if (student == null) {
+                throw new StudentException("Student not found with studentId: " + studentId);
+            }
+            return student;
+        } catch (Exception e) {
+            throw new StudentException("Failed to retrieve student with studentId: " + studentId);
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    @Override
+    public List<Student> getAllActiveStudents() throws StudentException {
+        EntityManager entityManager = EMUtil.provideEntityManager();
+
+        try {
+            TypedQuery<Student> query = entityManager.createQuery("SELECT s FROM Student s WHERE s.status = true", Student.class);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new StudentException("Failed to retrieve all active students.");
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    @Override
+    public College getCollegeFromStudentId(int studentId) throws CollegeException, StudentException {
+        EntityManager entityManager = EMUtil.provideEntityManager();
+
+        try {
+            Student student = entityManager.find(Student.class, studentId);
+            if (student == null) {
+                throw new StudentException("Student not found with studentId: " + studentId);
+            }
+            return student.getCollege();
+        } catch (StudentException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new CollegeException("Failed to retrieve college from student with studentId: " + studentId);
+        } finally {
+            entityManager.close();
+        }
+    }
 }
+
